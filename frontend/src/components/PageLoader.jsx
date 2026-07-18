2ce895329895afc401c5e7acd9d5b0289cb0653c
srikanth-thangamuthu <srikanth82146@gmail.com>
2026-07-18 20:06:40 +0530

    Delete frontend directory

--- frontend/src/components/PageLoader.jsx
+++
@@ -1,9 +0,0 @@
-import { LoaderIcon } from "lucide-react";
-function PageLoader() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-[#040912]">
-      <LoaderIcon className="size-10 animate-spin text-[#7dd3fc]" />
-    </div>
-  );
-}
-export default PageLoader;
